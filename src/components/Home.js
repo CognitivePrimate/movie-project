ac4e2cc5fb22c31e7806889ca955f22201bdf168
Derek Jackson <d4jackson84@gmail.com>
2021-07-13 20:03:27 -0400

    adding grid functionality to API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,53 @@
-import React, {useState} from "react";
+import React from "react";
+
+
 // config
 import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from "../config";
 // components
+import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+import Thumb from "./Thumb"
+import Spinner from "./Spinner"
 
-// hook
+// custom hook
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
 // image
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    
+    const { state, loading, error} = useHomeFetch();
+    console.log(state);
 
-    return <div>Home Page</div>
+    return (
+        <>
+        {state.results[0] ?
+        <HeroImage 
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`} 
+            title={state.results[0].original_title}
+            text={state.results[0].overview}
+
+        />
+        : null}
+        <Grid header="Popular Movies">
+            {state.results.map(movie => (
+                <Thumb
+                    key={movie.id}
+                    clickable
+                    image={
+                        movie.poster_path
+                        ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                        : NoImage
+                    
+                    }
+                    movieId={movie.id}
+                />
+            ))}
+        </Grid>
+        <Spinner />
+        </>
+    )
 }
 
 
